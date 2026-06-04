589e9a8393863eeef940391828bafeaf3a06c9fb
chandan-st <187197735+chandan-st@users.noreply.github.com>
2026-06-04 21:39:03 +0530

    Temporary CORS test

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ model = load_model()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://dragon-fruit-ai-2.onrender.com"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
